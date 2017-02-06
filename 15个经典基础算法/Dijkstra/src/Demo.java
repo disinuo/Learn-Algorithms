929e2c168750ea92d0209625eb939509a3f5d094
disinuo <776550026@qq.com>
2017-02-06 09:15:49 +0800

    提交竟然错了。但是也不显示在那个测试用例错的。。。

---
+++ 15个经典基础算法/Dijkstra/src/Demo.java
@@ -0,0 +1,9 @@
+/**
+ * Created by disinuo on 16/12/12.
+ */
+public class Demo {
+    public static void main(String[] a) {
+        // write your code here
+        System.out.print("jjj");
+    }
+}
